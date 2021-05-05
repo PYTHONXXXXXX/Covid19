b53a8e4e878963c7da705a8e806c838bccee7c0a
CuteDogLucky <66660541+CuteDogLucky@users.noreply.github.com>
2021-05-05 14:13:09 +0800

    Update Deletion.py

--- Class/Deletion.py
+++ Class/Deletion.py
@@ -1,10 +1,1 @@
-from Class.Sqlite import Sqlite
-class Deletion(Sqlite):
-    def __init__(self,db_name,table_name,delete_data):
-        super().__init__(db_name,table_name)
-        self.table_name=table_name
-        self.delete_data=delete_data
-    def delete(self):
-        pass
-    def execute(self):
-        pass
+
